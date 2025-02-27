fe35654ba5aa1378a7bc09f9496ba4e154a17f73
CesarFSN <fersana24@outlook.com>
2025-02-27 15:47:32 -0700

    Initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,140 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Programación
+ * Fecha: 27/02/2025
+ * Hora: 01:19 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+namespace Password_SNCF
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button BtnAceptar;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox TxtUsuario;
+		private System.Windows.Forms.TextBox TxtContras;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label2;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.BtnAceptar = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.TxtUsuario = new System.Windows.Forms.TextBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.TxtContras = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// BtnAceptar
+			// 
+			this.BtnAceptar.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.BtnAceptar.Location = new System.Drawing.Point(74, 207);
+			this.BtnAceptar.Name = "BtnAceptar";
+			this.BtnAceptar.Size = new System.Drawing.Size(75, 23);
+			this.BtnAceptar.TabIndex = 0;
+			this.BtnAceptar.Text = "&Aceptar";
+			this.BtnAceptar.UseVisualStyleBackColor = true;
+			this.BtnAceptar.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.Maroon;
+			this.label1.Location = new System.Drawing.Point(49, 97);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 23);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Usuario";
+			this.label1.Click += new System.EventHandler(this.Label1Click);
+			// 
+			// TxtUsuario
+			// 
+			this.TxtUsuario.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.TxtUsuario.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.TxtUsuario.Location = new System.Drawing.Point(112, 94);
+			this.TxtUsuario.Name = "TxtUsuario";
+			this.TxtUsuario.Size = new System.Drawing.Size(100, 22);
+			this.TxtUsuario.TabIndex = 3;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(239, 32);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(227, 209);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 5;
+			this.pictureBox1.TabStop = false;
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.ForeColor = System.Drawing.Color.Maroon;
+			this.label2.Location = new System.Drawing.Point(18, 147);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(88, 23);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "Contraseña";
+			// 
+			// TxtContras
+			// 
+			this.TxtContras.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.TxtContras.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.TxtContras.Location = new System.Drawing.Point(112, 144);
+			this.TxtContras.Name = "TxtContras";
+			this.TxtContras.PasswordChar = '*';
+			this.TxtContras.Size = new System.Drawing.Size(100, 22);
+			this.TxtContras.TabIndex = 4;
+			this.TxtContras.UseSystemPasswordChar = true;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+			this.ClientSize = new System.Drawing.Size(478, 276);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.TxtContras);
+			this.Controls.Add(this.TxtUsuario);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.BtnAceptar);
+			this.HelpButton = true;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "MainForm";
+			this.Text = "Control Escolar";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Programación
+ * Fecha: 27/02/2025
+ * Hora: 01:19 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Password_SNCF
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+	
+		}
+		void Label1Click(object sender, EventArgs e)
+		{
+	
+		}
+	}
+}
